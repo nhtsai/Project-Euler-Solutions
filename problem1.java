f3a2e6c486ee8ab5e211ecace2f2329aef72d288
Nathan Tsai (nhtsai) <natesigh@gmail.com>
2017-02-20 21:08:13 -0800

    Revise Problems 1 and 2
    Added to descriptions of Problems 1 and 2

--- problem1.java
+++ problem1.java
@@ -2,7 +2,7 @@
  * @author Nathan Tsai
  * @version 9/13/2016
  * 
- * Multiples of 3 and 5:
+ * Project Euler Problem 1 - Multiples of 3 and 5:
  * If we list all the natural numbers below 10 
  * that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
  * The sum of these multiples is 23.
