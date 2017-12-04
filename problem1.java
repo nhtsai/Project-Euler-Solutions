b033d89bc7fa2084712ab1c76576d63a8c1c3404
Nathan Tsai <natesigh@gmail.com>
2017-12-04 11:16:15 -0800

    Change problems 1, 2, and 3
    Revised the comments and clarity of problems 1, 2, and 3.

--- problem1.java
+++ problem1.java
@@ -1,40 +1,53 @@
 /*
- * @author Nathan Tsai
- * @version 9/13/2016
- * 
  * Project Euler Problem 1 - Multiples of 3 and 5:
- * If we list all the natural numbers below 10 
- * that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * If we list all the natural numbers below 10
+ * that are multiples of 3 or 5, we get 3, 5, 6 and 9.
  * The sum of these multiples is 23.
  * Find the sum of all the multiples of 3 or 5 below 1000.
-*/
-
+ *
+ * @author  Nathan Tsai
+ * @version 1.0, 3 Dec 2017
+ * @since   13 Sep 2016
+ */
 public class problem1 {
-  
-  public static void main(String[] args) {
-    
-    // prints sum of multiples of 3 or 5 under 10
-    System.out.println(multiples(10));
 
-    // prints sum of multiples of 3 or 5 under 1000
-    System.out.println(multiples(1000));
+  /**
+   * Runs the program and prints the solution of the problem to System.out.
+   *
+   * @param args the input taken in from the command line during runtime
+   */
+  public static void main(String[] args) {
+
+    // Prints sum of multiples of 3 or 5 under 10
+    // System.out.println(multiples(10));
+
+    // Prints sum of multiples of 3 or 5 under 1000
+    System.out.println(
+      "The sum of all multiples of 3 or 5 below 1,000 is: " + multiples(1000));
   }
-  
+
+  /**
+   * Returns the sum of the multiples of 3 or 5 under the specified integer
+   * parameter.
+   *
+   * @param  n the integer bound under which to find multiples of 3 or 5
+   * @return   the sum of the multiples of 3 or 5
+   */
   public static int multiples(int n)
   {
-    // initialzes sum
+    // Initialzes sum
     int sum = 0;
 
-    // adds the multiples of 3 or 5 under n to sum
+    // Iterates through all numbers from [0,n).
     for (int i = 0; i < n; i++) {
-      
+
+      // Adds number to sum if it is a multiple of 3 or 5
       if ((i % 3 == 0) || (i % 5 == 0)) {
         sum += i;
       }
-
    }
 
-   // returns final sum 
+   // Returns final sum
    return sum;
   }
-}
+} // End class problem1
--- problem3.java
+++ problem3.java
@@ -1,69 +1,96 @@
 /*
- * @author Nathan Tsai
- * @version 2/20/2017
- *
  * Project Euler Problem 3 - Largest Prime Factor:
  * The prime factors of 13195 are 5, 7, 13 and 29.
  * What is the largest prime factor of the number 600851475143?
+ *
+ * @author  Nathan Tsai
+ * @version 1.0, 3 Dec 2017
+ * @since   20 Feb 2017
  */
-
 public class problem3 {
-	
+
+	/**
+	 * Runs the program and prints the solution of the problem to System.out.
+	 *
+	 * @param args the input taken in from the command line during runtime
+	 */
 	public static void main(String[] args) {
-		//System.out.println(primeFac(600851475143));
-		System.out.println("The largest prime factor of the number 600,851,475,143 is: " + primeFac(600851475143L));
+
+		// Prints out the largest prime factor under 600,851,475,143
+		System.out.println(
+			"The largest prime factor of the number 600,851,475,143 is: " +
+			primeFac(600851475143L));
 	}
 
+	/**
+	 * Returns the largest prime factor under the specified bound a process
+	 * similar to prime factorization.
+	 *
+	 * @param  n the bound under which to find the largest prime factor
+	 * @return   the largest prime factor
+	 */
 	public static long primeFac(long n) {
 
-		// using a process similar to prime factorization
-		// using factoredNum to represent quotient after original number n is divided by incremented counter c
+		// Represents quotient after original number n is divided by c
 		long factoredNum = n;
-		long largestFactor = 0;
-		
-		// dividing by prime factors, starting with 2
-		int c = 2;
 
+		// Initializes the largest factor variable
+		long largestFactor = 0;
+
+		// Divides by prime factors, starting with 2
+		int c = 2;
 
 		while (c * c <= factoredNum) {
 
-			// checking if current c is a factor
+			// Checks if c is a factor
 			if (factoredNum % c == 0) {
-				
-				// dividing by that factor
+
+				// Divides number by factor c
 				factoredNum = factoredNum / c;
+
+				// Sets largest prime factor to c
 				largestFactor = c;
 			}
 			else {
 
-				// incrementing c by 1 to check for other prime factors
-				c++;
+				// Increments c to check for other prime factors
+				do {
+					c++;
+				} while (!isPrime((long) c));
 			}
 		}
 
-		// if the remaining factored number is greater than the largest factor, the remaining factored number is now the largest prime factor
+		// Sets the remaining factored number to be the largest prime factor
+		// if the remaining factored number is greater than the largest prime factor
 		if (factoredNum > largestFactor) {
 			largestFactor = factoredNum;
 		}
 
-		// return the largest prime factor of n
+		// Return the largest prime factor of n
 		return largestFactor;
 	}
 
+	/**
+	 * Returns a boolean if the specified number is prime (true) or not (false).
+	 *
+	 * @param  n the number to check if it is prime
+	 * @return   the boolean true or false if the number is prime or not
+	 */
 	public static boolean isPrime(long n) {
-		// checking if n is even
+
+		// Checks if n is even
 		if (n % 2 == 0) {
 			return false;
 		}
 
-		// checking all other odd factors up to square root
-		for (int i = 3; i*i <= n; i+=2) {
+		// Checks all other odd factors up to square root
+		for (int i = 3; i * i <= n; i += 2) {
 			if (n % i == 0) {
 				return false;
 			}
 		}
 
-		// return true if n is prime
+		// Returns true if n is prime
 		return true;
 	}
 
