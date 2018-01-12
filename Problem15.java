26499827d9c3276dd33df1a734554d7a0f15ca50
Nathan Tsai <natesigh@gmail.com>
2018-01-11 21:47:12 -0800

    Initial Problem 15 commit.
    Unfinished, worked on setting up problem 15.

---
+++ Problem15.java
@@ -0,0 +1,42 @@
+/**
+ * Project Euler Problem 15 - Lattice Paths:
+ * Starting in the top left corner of a 2×2 grid,
+ * and only being able to move to the right and down,
+ * there are exactly 6 routes to the bottom right corner.
+ *
+ * How many such routes are there through a 20×20 grid?
+ *
+ * @author  Nathan Tsai
+ * @version 1.0, 11 Jan 2018
+ * @since   7 Jan 2018
+ */
+
+public class Problem15 {
+
+  /**
+   * Runs the program and prints the solution of the problem to System.out.
+   *
+   * @param args the input taken in from the command line during runtime
+   */
+  public static void main(String[] args) {
+    System.out.println("There are %d routes through a 2 by 2 grid.\n",
+      numRoutes(2));
+    // System.out.println("There are %d routes through a 20 by 20 grid.\n",
+    //   numRoutes(20));
+  }
+
+  /**
+   * Returns the number of routes from the top left corner to the bottom right
+   * corner of a square lattice of the specified dimension by ..... .
+   *
+   * @param  dimension the dimension of the square lattice
+   * @return           the
+   */
+  public static int numRoutes(int dimension) {
+    int routes = 0;
+
+    while ()
+
+    return routes;
+  }
+} // End class Problem15
